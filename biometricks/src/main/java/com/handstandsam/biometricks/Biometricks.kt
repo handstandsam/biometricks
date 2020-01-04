f590f043a6f66e2762744acef85a8426303771a6
Evan Tatarka <evan@tatarka.me>
2020-01-04 18:31:44 -0500

    Add wrapper for showing the biometric prompt

--- biometricks/src/main/java/com/handstandsam/biometricks/Biometricks.kt
+++ biometricks/src/main/java/com/handstandsam/biometricks/Biometricks.kt
@@ -1,7 +1,21 @@
 package com.handstandsam.biometricks
 
+import android.app.Activity
 import android.content.Context
+import android.os.Build
+import android.os.Handler
+import android.os.Looper
+import android.view.ViewTreeObserver
+import androidx.annotation.MainThread
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.handstandsam.biometricks.internal.BiometricksHelper
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
+import kotlin.coroutines.suspendCoroutine
 
 /**
  * Biometricks is the type of Biometric Authentication that
@@ -47,6 +61,231 @@ sealed class Biometricks {
          */
         fun from(context: Context): Biometricks {
             return BiometricksHelper(context).type
+        }
+
+        /**
+         * Wrapper around [BiometricPrompt.authenticate] which handles many tricky issues for you.
+         * It will:
+         * - Wait for the app to be focused if not currently, as the prompt will fail to show
+         * otherwise.
+         * - Allow you to show/hide a loading indicator for when there's a delay showing the prompt.
+         * - Give you information on if you need to show an error to the user or not.
+         * ```
+         * lifecycleScope.launch {
+         *     try {
+         *         val unlockedCryptObject = Biometricks.showPrompt(
+         *             this@MainActivity,
+         *             lockedCryptoObject,
+         *             BiometricPrompt.PromptInfo.Builder()
+         *                 .setTitle(...)
+         *                 .setNegativeButtonText(...)
+         *                 .build()
+         *         ) { showLoading -> progressBar.visibility = if (showLoading) View.VISIBLE else View.INVISIBLE }
+         *
+         *         // success
+         *     } catch (e: BiometricException) {
+         *         // failure
+         *         if (e.shouldShow) {
+         *             // show error to the user
+         *         }
+         *     }
+         * }
+         * ```
+         *
+         * @param activity The host activity.
+         * @param cryptoObject The [BiometricPrompt.CryptoObject] to unlock. This is always required
+         * as you many not get 'secure' biometrics if you don't include it.
+         * @param promptInfo The [BiometricPrompt.PromptInfo] to display in the prompt.
+         * @param showLoading Callback to show/hide a loading indicator for when there's a delay
+         * showing the prompt. This is only used on api 28 as after that it shows immediately.
+         */
+        @MainThread
+        suspend fun showPrompt(
+            activity: FragmentActivity,
+            cryptoObject: BiometricPrompt.CryptoObject,
+            promptInfo: BiometricPrompt.PromptInfo,
+            showLoading: (Boolean) -> Unit
+        ): BiometricPrompt.CryptoObject {
+            ensureFocus(activity)
+
+            return handleApi28Loading(activity, showLoading) {
+                suspendCoroutine<BiometricPrompt.CryptoObject> { continuation ->
+                    BiometricPrompt(
+                        activity,
+                        ContextCompat.getMainExecutor(activity),
+                        Callback(continuation)
+                    ).authenticate(promptInfo, cryptoObject)
+                }
+            }
+        }
+
+        /**
+         * Wrapper around [BiometricPrompt.authenticate] which handles many tricky issues for you.
+         * It will:
+         * - Wait for the app to be focused if not currently, as the prompt will fail to show
+         * otherwise.
+         * - Allow you to show/hide a loading indicator for when there's a delay showing the prompt.
+         * - Give you information on if you need to show an error to the user or not.
+         * ```
+         * lifecycleScope.launch {
+         *     try {
+         *         val unlockedCryptObject = Biometricks.showPrompt(
+         *             this@MainActivity,
+         *             lockedCryptoObject,
+         *             BiometricPrompt.PromptInfo.Builder()
+         *                 .setTitle(...)
+         *                 .setNegativeButtonText(...)
+         *                 .build()
+         *         ) { showLoading -> progressBar.visibility = if (showLoading) View.VISIBLE else View.INVISIBLE }
+         *
+         *         // success
+         *     } catch (e: BiometricException) {
+         *         // failure
+         *         if (e.shouldShow) {
+         *             // show error to the user
+         *         }
+         *     }
+         * }
+         * ```
+         *
+         * @param fragment The host fragment.
+         * @param cryptoObject The [BiometricPrompt.CryptoObject] to unlock. This is always required
+         * as you many not get 'secure' biometrics if you don't include it.
+         * @param promptInfo The [BiometricPrompt.PromptInfo] to display in the prompt.
+         * @param showLoading Callback to show/hide a loading indicator for when there's a delay
+         * showing the prompt. This is only used on api 28 as after that it shows immediately.
+         */
+        @MainThread
+        suspend fun showPrompt(
+            fragment: Fragment,
+            cryptoObject: BiometricPrompt.CryptoObject,
+            promptInfo: BiometricPrompt.PromptInfo,
+            showLoading: (Boolean) -> Unit
+        ): BiometricPrompt.CryptoObject {
+            val activity = fragment.requireActivity()
+
+            ensureFocus(activity)
+
+            return handleApi28Loading(activity, showLoading) {
+                suspendCoroutine<BiometricPrompt.CryptoObject> { continuation ->
+                    BiometricPrompt(
+                        fragment,
+                        ContextCompat.getMainExecutor(fragment.requireContext()),
+                        Callback(continuation)
+                    ).authenticate(promptInfo, cryptoObject)
+                }
+            }
+        }
+
+        private suspend fun <T> handleApi28Loading(
+            activity: Activity,
+            showLoading: (Boolean) -> Unit,
+            showPrompt: suspend () -> T
+        ): T {
+            // On api 28 if the user is locked out of biometrics from too many failed
+            // attempts there will be a long delay before getting the error back. So the
+            // user isn't confused as to what is going on, show a loading indicator.
+            if (Build.VERSION.SDK_INT == 28) {
+                val window = activity.window
+                val handler = Handler(Looper.getMainLooper())
+
+                var showingLoading = false
+
+                // Don't show if the prompt shows quickly
+                val showLoadingRunnable = Runnable {
+                    showingLoading = true
+                    showLoading(true)
+                }
+                handler.postDelayed(showLoadingRunnable, 150)
+
+                // The only way to tell that the prompt is shown is to listen to the window
+                // losing focus events.
+                window.decorView.viewTreeObserver.addOnWindowFocusChangeListener(object :
+                    ViewTreeObserver.OnWindowFocusChangeListener {
+                    override fun onWindowFocusChanged(hasFocus: Boolean) {
+                        if (!hasFocus) {
+                            window.decorView.viewTreeObserver.removeOnWindowFocusChangeListener(this)
+                            handler.removeCallbacks(showLoadingRunnable)
+                            showingLoading = false
+                            showLoading(false)
+                        }
+                    }
+                })
+
+                try {
+                    return showPrompt()
+                } finally {
+                    handler.removeCallbacks(showLoadingRunnable)
+                    // Hide loading if the prompt fails to show
+                    if (showingLoading) {
+                        showLoading(false)
+                    }
+                }
+            } else {
+                // The prompt shows immediately on api 29+ so don't worry about any of this.
+                return showPrompt()
+            }
+        }
+
+        private suspend fun ensureFocus(activity: Activity) {
+            suspendCoroutine<Unit> { continuation ->
+                // Showing the biometrics prompt will be ignored if the app does not have focus. You may
+                // think that this will always be the case if you are resumed, it is not.
+                if (activity.hasWindowFocus()) {
+                    continuation.resume(Unit)
+                } else {
+                    val window = activity.window
+                    window.decorView.viewTreeObserver.addOnWindowFocusChangeListener(object :
+                        ViewTreeObserver.OnWindowFocusChangeListener {
+                        override fun onWindowFocusChanged(hasFocus: Boolean) {
+                            if (hasFocus) {
+                                window.decorView.viewTreeObserver.removeOnWindowFocusChangeListener(
+                                    this
+                                )
+                                continuation.resume(Unit)
+                            }
+                        }
+                    })
+                }
+            }
+        }
+
+        private class Callback(private val continuation: Continuation<BiometricPrompt.CryptoObject>) :
+            BiometricPrompt.AuthenticationCallback() {
+            private var authenticationFailed = false
+
+            override fun onAuthenticationError(
+                errorCode: Int,
+                errString: CharSequence
+            ) {
+                val shouldShow =
+                    !isCancel(errorCode) && !authenticationFailed
+                continuation.resumeWithException(
+                    BiometricException(
+                        errorCode,
+                        errString,
+                        shouldShow
+                    )
+                )
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                continuation.resume(result.cryptoObject!!)
+            }
+
+            override fun onAuthenticationFailed() {
+                // This means the dialog was shown, so we don't want to show the
+                // error again ourselves
+                authenticationFailed = true
+            }
+
+            /**
+             * If the prompt was canceled we don't want to show an error ourselves
+             */
+            fun isCancel(errorCode: Int) =
+                errorCode == BiometricPrompt.ERROR_CANCELED
+                        || errorCode == BiometricPrompt.ERROR_USER_CANCELED
+                        || errorCode == BiometricPrompt.ERROR_NEGATIVE_BUTTON
         }
     }
 }
--- sample/src/main/java/com/handstandsam/biometricks/sample/MainActivity.kt
+++ sample/src/main/java/com/handstandsam/biometricks/sample/MainActivity.kt
@@ -1,50 +1,31 @@
 package com.handstandsam.biometricks.sample
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
 import androidx.biometric.BiometricPrompt
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
+import com.handstandsam.biometricks.BiometricException
 import com.handstandsam.biometricks.Biometricks
 import com.handstandsam.biometricks.Biometricks.Available
 import com.handstandsam.biometricks.Biometricks.Available.*
-import java.util.concurrent.Executors
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : FragmentActivity() {
-
-    private val biometricAuthenticationCallback = object : BiometricPrompt.AuthenticationCallback() {
-
-        override fun onAuthenticationError(
-            errorCode: Int,
-            errString: CharSequence
-        ) {
-            showToast("Error | Message: $errString | Code: $errorCode")
-        }
-
-        /**
-         * Called when a biometric is recognized.
-         *
-         * @param result An object containing authentication-related data
-         */
-        override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
-            showToast("Succeeded")
-        }
-
-        /**
-         * Called when a biometric is valid but not recognized.
-         */
-        override fun onAuthenticationFailed() {
-            showToast("Failed")
-        }
-    }
 
     private val biometrick: Biometricks by lazy { Biometricks.from(applicationContext) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        findViewById<TextView>(R.id.biometrick_type).text = "Type: ${biometrick::class.java.simpleName}"
+        findViewById<TextView>(R.id.biometrick_type).text =
+            "Type: ${biometrick::class.java.simpleName}"
         when (biometrick) {
             is Available -> {
                 findViewById<Button>(R.id.launch_biometric)
@@ -74,18 +55,32 @@ class MainActivity : FragmentActivity() {
                 RuntimeException("Shouldn't get here... You should show another UI if the user doesn't have Biometrics")
             }
         }
-        BiometricPrompt(
-            this,
-            Executors.newSingleThreadExecutor(),
-            biometricAuthenticationCallback
-        )
-            .authenticate(
-                BiometricPrompt.PromptInfo.Builder()
-                    .setTitle("Authenticate with $biometricName")
-                    .setNegativeButtonText("Cancel")
-                    .setDeviceCredentialAllowed(false)
-                    .build()
-            )
+        lifecycleScope.launch {
+            val cryptoObject = withContext(Dispatchers.IO) {
+                Crypto().cryptoObject()
+            }
+
+            try {
+                val unlockedCryptoObject = Biometricks.showPrompt(
+                    this@MainActivity,
+                    cryptoObject,
+                    BiometricPrompt.PromptInfo.Builder()
+                        .setTitle("Authenticate with $biometricName")
+                        .setNegativeButtonText("Cancel")
+                        .setDeviceCredentialAllowed(false)
+                        .build()
+                ) { showLoading ->
+                    findViewById<View>(R.id.loading).visibility =
+                        if (showLoading) View.VISIBLE else View.INVISIBLE
+                }
+
+                showToast("Succeeded")
+            } catch (e: BiometricException) {
+                if (e.shouldShow) {
+                    showToast("Error | Message: ${e.errString} | Code: ${e.code}")
+                }
+            }
+        }
     }
 
     private fun showToast(message: String) {
